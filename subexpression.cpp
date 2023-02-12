cda7a0cf31a4399f01320776aab9ff6f159638aa
alroney <andrewroney@rocketmail.com>
2023-02-12 17:41:03 -0500

    -Restart

--- subexpression.cpp
+++ subexpression.cpp
@@ -17,12 +17,6 @@ using namespace std;
 #include "operand.h"
 #include "plus.h"
 #include "minus.h"
-#include "times.h"
-#include "divide.h"
-#include "modulo.h"
-#include "power.h"
-#include "lessThan.h"
-#include "greaterThan.h"
 
 SubExpression::SubExpression(Expression* left, Expression* right) {
     this->left = left;
@@ -33,30 +27,17 @@ Expression* SubExpression::parse(stringstream& in) {
     Expression* left;
     Expression* right;
     char operation, paren;
-
+    
     left = Operand::parse(in);
     in >> operation;
     right = Operand::parse(in);
     in >> paren;
-
     switch (operation) {
         case '+':
             return new Plus(left, right);
         case '-':
             return new Minus(left, right);
         //region - ADDED - added by me
-        case '*':
-            return new Times(left, right);
-        case '/':
-            return new Divide(left, right);
-        case '%':
-            return new Modulo(left, right);
-        case '^':
-            return new Power(left, right);
-        case '<':
-            return new LessThan(left, right);
-        case '>':
-            return new GreaterThan(left, right);
         //endregion
     }
     return 0;
